714df445e8f83bbadeca88ae835615d0c708d222
planetabhijit <abhijitpatelraju@gmail.com>
2022-12-15 21:26:05 +0530

    initial commit for simplecamelspringboot

---
+++ src/test/java/com/decodedbytes/SimpleCamelSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decodedbytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCamelSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
